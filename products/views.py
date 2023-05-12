83aae6151f59a28ab245a744c20448523b38468b
dimadimontaj <drudakov03@mail.ru>
2023-05-13 02:13:34 +0300

    20

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 from products.models import ProductCategory, Product, Basket
 from users.models import User
 
-# Create your views here.
 
 def index(request):
     context = {'title': 'Store'}
@@ -18,6 +18,7 @@ def products(request):
     return render(request, 'products/products.html', context)
 
 
+@login_required()
 def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
@@ -32,6 +33,7 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META['HTTP_REFERER'])
 
 
+@login_required()
 def basket_remove(request, basket_id):
     basket = Basket.objects.get(id=basket_id)
     basket.delete()
